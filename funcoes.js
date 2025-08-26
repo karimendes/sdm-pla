9735f58b600a90c475ea418242dc11fec4e03764
Karine F. Mendes <karineferreiramendes@gmail.com>
2025-08-25 21:42:27 -0300

    Create funcoes.js

---
+++ funcoes.js
@@ -0,0 +1,26 @@
+//função declarativa
+function saudacao(nome) {
+    return `olá, ${nome}!`
+}
+
+console.log(saudacao("kari"))
+
+
+//função anônima
+const soma = function(a,b) {
+    return a + b;
+}
+
+function simples(){
+let a = 5
+let b = 12
+let c = a + b
+}
+
+console.log("o valor de c é: " + c)
+
+function somar (a,b){
+    return a + b;
+}
+
+console.log( somar(5,10))
