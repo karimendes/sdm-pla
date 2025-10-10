ef767ec1e94e96a9f9a72753d187474613b436b8
Karine Ferreira Mendes Andrade Silva <824124743@ulife.com.br>
2025-10-10 11:39:21 -0300

    get user atualizado

--- user-service/userService.js
+++ user-service/userService.js
@@ -49,6 +49,17 @@ app.post('/usuarios', async (req, res) => {
     }
 });
 
+app.get('/usuarios', async (req, res) => {
+    try {
+        const users = await User.find().sort({createdAt: -1})
+            return res.json(users)
+        } catch(err) {
+            console.error(err)
+            return res.status(500).json({error: "erro ao buscar usuário"})
+        }
+    }
+)
+
 app.listen(3000, () => {
-    console.log('Order service running on port 3000');
+    console.log('User service running on port 3000');
 });
